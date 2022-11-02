06d71def9159819cb45a33d695189ef07d7d913f
ochom <ochomrichard752@gmail.com>
2022-11-02 10:48:56 +0300

    new changes

--- main.go
+++ main.go
@@ -1,1 +1,8 @@
-package repository
+package database
+
+import "github.com/ochom/orctic-database/repository"
+
+// New ...
+func New(dbPlatform repository.Platform) (repository.Repo, error) {
+	return repository.New(dbPlatform)
+}
