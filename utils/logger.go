ebe553c31c3f42783a43a737332d40396ea4d4f3
ochom <ochomrichard752@gmail.com>
2023-02-16 10:17:42 +0300

    fix: logger

--- utils/logger.go
+++ utils/logger.go
@@ -16,28 +16,27 @@ func NewLogger() *Logger {
 	return &Logger{log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)}
 }
 
-// Print ...
-func (c *Logger) Print(txt string) {
-	_ = c.k.Output(3, txt)
+func (c *Logger) print(s string) {
+	_ = c.k.Output(3, s)
 }
 
 // Info ...
-func (c *Logger) Info(v ...any) {
-	c.Print("INFO: " + fmt.Sprint(v...))
+func (c *Logger) Info(s string, v ...any) {
+	c.print("INFO: " + fmt.Sprintf(s, v...))
 }
 
 // Warn ...
-func (c *Logger) Warn(v ...any) {
-	c.Print("WARN: " + fmt.Sprint(v...))
+func (c *Logger) Warn(s string, v ...any) {
+	c.print("WARN: " + fmt.Sprintf(s, v...))
 }
 
 // Error ...
-func (c *Logger) Error(v ...any) {
-	c.Print("ERROR: " + fmt.Sprint(v...))
+func (c *Logger) Error(s string, v ...any) {
+	c.print("ERROR: " + fmt.Sprintf(s, v...))
 }
 
 // Fatal ...
-func (c *Logger) Fatal(v ...any) {
-	c.Print("FATAL: " + fmt.Sprint(v...))
+func (c *Logger) Fatal(s string, v ...any) {
+	c.print("FATAL: " + fmt.Sprintf(s, v...))
 	os.Exit(1)
 }
