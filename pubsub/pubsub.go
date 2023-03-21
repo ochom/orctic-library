fe79b0588753ec6fa16a530e989e301e90a487c9
ochom <ochomrichard752@gmail.com>
2023-03-21 16:58:58 +0300

    update launch

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -4,8 +4,6 @@ import (
 	"time"
 
 	"github.com/ochom/orctic-library/utils"
-	"github.com/ochom/pubsub"
-	"github.com/ochom/quickmq/clients"
 )
 
 // Pubsub is an interface for pubsub implementations
@@ -17,61 +15,12 @@ type Pubsub interface {
 	Subscribe(channel string, handler func([]byte)) error
 }
 
-// QuickMQ is a quickmq implementation of the Pubsub interface
-type QuickMQ struct {
-	url string
-}
-
-// NewQuickMQ returns a new QuickMQ instance
-func NewQuickMQ() *QuickMQ {
-	quickURL := utils.GetEnv("QUICK_URL", "ws://localhost:3456")
-	return &QuickMQ{
-		url: quickURL,
-	}
-}
-
-// Publish publishes a message to a channel
-func (q *QuickMQ) Publish(channel string, message []byte, delay time.Duration) error {
-	p := clients.NewPublisher(q.url, channel)
-	if delay == 0 {
-		return p.Publish(message)
+// New returns a new Pubsub instance
+func New() Pubsub {
+	provider := utils.GetEnv("PUBSUB_PROVIDER", "rabbitmq")
+	if provider == "quickmq" {
+		return NewRabbitMQ()
 	}
 
-	return p.PublishWithDelay(message, delay)
-}
-
-// Subscribe subscribes to a channel
-func (q *QuickMQ) Subscribe(channel string, handler func([]byte)) error {
-	c := clients.NewConsumer(q.url, channel)
-	return c.Consume(handler)
-}
-
-// RabbitMQ is a rabbitmq implementation of the Pubsub interface
-type RabbitMQ struct {
-	url string
-}
-
-// NewRabbitMQ returns a new RabbitMQ instance
-func NewRabbitMQ() *RabbitMQ {
-	rabbitURL := utils.GetEnv("RABBIT_URL", "amqp://guest:guest@localhost:5672/")
-	return &RabbitMQ{
-		url: rabbitURL,
-	}
-}
-
-// Publish publishes a message to a channel
-func (r *RabbitMQ) Publish(channel string, message []byte, delay time.Duration) error {
-	p := pubsub.NewPublisher(r.url, channel)
-	if delay == 0 {
-
-		return p.Publish(message)
-	}
-
-	return p.PublishWithDelay(message, delay)
-}
-
-// Subscribe subscribes to a channel
-func (r *RabbitMQ) Subscribe(channel string, handler func([]byte)) error {
-	c := pubsub.NewConsumer(r.url, channel)
-	return c.Consume(handler)
+	return NewRabbitMQ()
 }
